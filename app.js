6c4bfa2c9c76d427cfd642c15ec36f923b630f0a
PaingSC <paingsoechit.job@gmail.com>
2025-03-20 23:07:29 +0700

    API: handling get reqquest

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 // import express from 'express';
+const fs = require('fs');
 const express = require('express');
+const { dirname } = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
-});
+// I. Basic routing with 'express'
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint...');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    tours,
+  });
 });
 
 const port = 3000;
