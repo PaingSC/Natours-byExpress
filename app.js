7d051db20f7a60742d6449fec172dc89c22bd952
PaingSC <paingsoechit.job@gmail.com>
2025-03-25 18:56:44 +0700

    Adding sub routers for users and tours

--- app.js
+++ app.js
@@ -141,27 +141,19 @@ const deleteUser = (req, res, next) => {
 };
 
 // 3 |Routes
-// app.get('/api/v1/tours', getAllTours);
-// app.post('/api/v1/tours', createTour);
-// app.get('/api/v1/tours/:id', getATour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
+// 3.1 |Tours Routes
+const tourRouter = express.Router();
+const userRouter = express.Router();
 
-// Tours Routes
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
-app
-  .route('/api/v1/tours/:id')
-  .get(getATour)
-  .patch(updateTour)
-  .delete(deleteTour);
+tourRouter.route('/').get(getAllTours).post(createTour);
+tourRouter.route('/:id').get(getATour).patch(updateTour).delete(deleteTour);
 
-// Users Routes
-app.route('/api/v1/users').get(getAllUsers).post(createUser);
-app
-  .route('/api/v1/users/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
+// 3.2 |Users Routes
+userRouter.route('/').get(getAllUsers).post(createUser);
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 // 4| Server
 const port = 3000;
