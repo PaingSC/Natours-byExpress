7ff38611ebd36a7e48a96010abfed9a41ef578e5
PaingSC <paingsoechit.job@gmail.com>
2025-03-26 10:37:54 +0700

    Chaining multiple middlewares | add checkBody middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -15,6 +15,16 @@ exports.checkID = (req, res, next, val) => {
   next();
 };
 
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'bad request',
+    });
+  }
+  next();
+};
+
 // Get all Tours (get request)
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
