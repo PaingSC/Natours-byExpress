54fabec2a5eb25dfd2cfd2637543691562beea10
PaingSC <paingsoechit.job@gmail.com>
2025-03-30 01:08:13 +0700

    Setting NODE_ENV

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const dotenv = require('dotenv');
-const app = require('./app');
-
 dotenv.config({ path: './config.env' });
 
-// console.log(app.get('env'));
-console.log(process.env);
+const app = require('./app');
+
+// console.log(process.env);
 
 // 4| Server
-const port = 3000;
+const port = process.env.PORT || 3000;
 const host = '127.0.0.1';
 app.listen(port, () => {
   console.log(`App is running on ${port}...`);
