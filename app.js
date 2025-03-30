5ff8b592ff290d5f84fe9d9256b38994afcd8398
PaingSC <paingsoechit.job@gmail.com>
2025-03-31 01:42:15 +0700

    Installing some dev-dependencies

--- app.js
+++ app.js
@@ -8,10 +8,15 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1| Middlewares
+// if (process.env.NODE_ENV === 'development') {
+//   app.use(morgan('dev'));
+//   console.log(process.env.NODE_ENV);
+// }
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
-  console.log(process.env.NODE_ENV);
 }
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
