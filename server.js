146682c7c986c89c37ff9804f77ff4340e35ac1c
PaingSC <paingsoechit.job@gmail.com>
2025-03-31 01:53:03 +0700

    editing '.eslint.json'

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+dotenv.config({ path: './config.env' });
 
 // console.log(process.env);
 
 // 4| Server
 const port = process.env.PORT || 3000;
-const host = '127.0.0.1';
+// const host = '127.0.0.1';
 app.listen(port, () => {
   console.log(`App is running on ${port}...`);
 });
