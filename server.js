5105639ed65ed1df92d222a1035f6e827bc3f7fc
PaingSC <paingsoechit.job@gmail.com>
2025-04-05 09:17:39 +0700

    installing mongoose

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    // .connect(process.env.DATABASE_LOCAL, { // For Local Database
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connection succesful!');
+  });
 
 // console.log(process.env);
 
