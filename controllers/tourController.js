6d51e7fd77608d98b9d609deae08313adf25d6e3
PaingSC <paingsoechit.job@gmail.com>
2025-04-24 00:47:03 +0700

    Modelling the Tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -89,7 +89,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'failed',
-      message: 'Invalid data sent!',
+      message: err,
     });
   }
 };
