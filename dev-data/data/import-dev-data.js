3481e80195edba75581d220b853f31c261250284
PaingSC <paingsoechit.job@gmail.com>
2025-04-24 10:25:49 +0700

    Adding a import-dev-data script

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const Tour = require('./../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {})
+
+  .then(() => console.log('DB connection successful!'))
+  .catch((err) => console.error('DB connection error:', err));
+
+// Read JSON file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+);
+
+// IMPORT DATA INTO DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// DELETE ALL DATAT FROM COLLECTION(DB)
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany(tours);
+    console.log('Data successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+console.log(process.argv);
