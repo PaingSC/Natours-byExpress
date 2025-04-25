c634525f8ba7427a30875023ca20ecbd1a2e7836
PaingSC <paingsoechit.job@gmail.com>
2025-04-25 09:38:27 +0700

    Making API Better Filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,7 +3,25 @@ const Tour = require('../models/tourModel');
 // Get all Tours (get request)
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    console.log(req.query, queryObj, req.params);
+
+    // EXECUTE QUERY
+    const query = Tour.find(queryObj);
+
+    // const query = Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
