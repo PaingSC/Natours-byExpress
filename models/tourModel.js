97fb10296ef1e35a057d4faf94b77e3bdd53d578
PaingSC <paingsoechit.job@gmail.com>
2025-05-13 17:05:07 +0700

    Working wiht document middleware with mongoose

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'Atour must have a duration'],
@@ -68,6 +70,24 @@ tourSchema.virtual('durationWeeks').get(function () {
 tourSchema.virtual('nameUppercase').get(function () {
   return this.name.toUpperCase();
 });
+
+// Document Middleware(preHook): runs before .save() and .create()
+tourSchema.pre('save', function (next) {
+  // console.log(this);
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre('save', function (next) {
+//   console.log('Will save document...');
+//   next();
+// });
+
+// postHooks
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
