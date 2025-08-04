22087f1f4a93e23eef8afce20059ea419f8286fd
PaingSC <paingsoechit.job@gmail.com>
2025-08-04 21:43:06 +0700

    Implementing rate limiting

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-// const rateLimit = require('express-rate-limit');
+const rateLimit = require('express-rate-limit');
 // const { dirname } = require('path');
 
 const tourRouter = require('./routes/tourRoutes');
@@ -19,6 +19,13 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'To many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
